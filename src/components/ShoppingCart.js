fbc2bd44f7aab841fb0eb25b5c15e668aa656f8d
taraslysun <taraslysun2@gmail.com>
2024-05-17 13:10:01 +0300

    added toasts

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -4,7 +4,7 @@ import ShoppingCartElement from "./ShoppingCartElement";
 import emailjs from "emailjs-com";
 import { get_stone_gallery } from "../firebase-communication/firebase-database";
 import { useTranslation } from "react-i18next";
-
+import { notifyError, notifySuccess } from "../toast-config";
 
 const serviceId = "service_2vm4ala";
 const templateId = "template_oksmh3k";
@@ -38,34 +38,45 @@ function ShoppingCart() {
   }, []);
 
   const formatDescription = (data) => {
-    return Object.entries(data).map(([index, item]) => {
-      const stone = stoneGallery.find((stone) => stone.id === item.stone);
-      return `Пункт ${parseInt(index) + 1}:
-      - Розмірність: ${item.measure || 'Не вказано'}
-      - Ширина: ${item.width || 'Не вказано'}
-      - Довжина: ${item.length || 'Не вказано'}
-      - Висота: ${item.height || 'Не вказано'}
-      - Камінь: ${stone?.title || 'Не вказано'}
-      - Розмірність кількості: ${item.quantity_measure || 'Не вказано'}
-      - Кількість: ${item.quantity || 'Не вказано'}
-      - Номер телефону: ${item.phoneNumber || 'Не вказано'}\n\n`;
-    }).join('');
+    return Object.entries(data)
+      .map(([index, item]) => {
+        const stone = stoneGallery.find((stone) => stone.id === item.stone);
+        return `Пункт ${parseInt(index) + 1}:
+      - Розмірність: ${item.measure || "Не вказано"}
+      - Ширина: ${item.width || "Не вказано"}
+      - Довжина: ${item.length || "Не вказано"}
+      - Висота: ${item.height || "Не вказано"}
+      - Камінь: ${stone?.title || "Не вказано"}
+      - Розмірність кількості: ${item.quantity_measure || "Не вказано"}
+      - Кількість: ${item.quantity || "Не вказано"}
+      - Номер телефону: ${item.phoneNumber || "Не вказано"}\n\n`;
+      })
+      .join("");
   };
 
   const handleSendEmail = () => {
     if (localStorage.getItem("cart") === null || orderedItems.length === 0) {
-      alert(t('shoppingCart.emptyCart'));
+      notifyError(t("shoppingCart.emptyCart"));
       return;
     }
     if (Object.keys(formData).length === 0) {
-      alert(t('shoppingCart.orderError'));
+      notifyError(t("shoppingCart.orderError"));
       return;
     }
     for (const key in formData) {
       console.log(formData[key], key);
       const item = formData[key];
-      if ((!item.measure) || (!item.width) || (!item.length) || (!item.height) || (!item.stone) || (!item.quantity_measure) || (!item.quantity) || (!item.phoneNumber)) {
-        alert(t('shoppingCart.orderError'));
+      if (
+        !item.measure ||
+        !item.width ||
+        !item.length ||
+        !item.height ||
+        !item.stone ||
+        !item.quantity_measure ||
+        !item.quantity ||
+        !item.phoneNumber
+      ) {
+        notifyError(t("shoppingCart.orderError"));
         return;
       }
     }
@@ -80,7 +91,7 @@ function ShoppingCart() {
       .send(serviceId, templateId, templateParams, userId)
       .then(
         (response) => {
-          alert(t('shoppingCart.orderSuccess'));
+          notifySuccess(t("shoppingCart.orderSuccess"));
           console.log("OK", response.status, response.text);
         },
         (error) => {
@@ -108,7 +119,7 @@ function ShoppingCart() {
         ))}
       </div>
       <button onClick={handleSendEmail} className="order-button">
-        {t('shoppingCart.orderButton')}
+        {t("shoppingCart.orderButton")}
       </button>
     </div>
   );
